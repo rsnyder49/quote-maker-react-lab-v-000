f13ca394c9555157a9e7e795a38e533cb533f2d0
Robert Snyder <r.snyder49@gmail.com>
2019-01-18 02:22:30 +0000

    Automatically backed up by Learn

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -19,11 +19,9 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {quotes.map((quote, index) => 
+                <QuoteCard key={index} quote={quote} 
+                 removeQuote={removeQuote} upvoteQuote={upvoteQuote} downvoteQuote={downvoteQuote} />)}
             </div>
           </div>
         </div>
