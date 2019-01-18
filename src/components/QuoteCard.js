28e1212b47a6f10a38f9a67d2c8788590e866bab
Robert Snyder <r.snyder49@gmail.com>
2019-01-18 02:14:24 +0000

    Automatically backed up by Learn

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -33,7 +33,7 @@ const QuoteCard = ({ quote, removeQuote, upvoteQuote, downvoteQuote }) =>
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div>Votes: {quote.votes}</div>
       </div>
     </div>
   </div>;
